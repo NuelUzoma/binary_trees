20db81a144f73bbbc669fe3e617380158dbc8544
NuelUzoma <emcool17@gmail.com>
2023-03-02 22:42:35 +0100

    Task 8

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,33 +2,48 @@
 #include <stdlib.h>
 
 /**
+ * check - checks for the height of the tree
+ * @node: the node to be checked
+ * @height: the height to be used
+ * Return: the height of the tree
+ */
+size_t check(const binary_tree_t *node, size_t height)
+{
+	size_t newHeight, leftHeight, rightHeight;
+
+	newHeight = 0;
+	leftHeight = 0;
+	rightHeight = 0;
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (height);
+	}
+	newHeight = height + 1;
+	if (node->left != NULL)
+	{
+		leftHeight = check(node->left, newHeight);
+	}
+	if (node->right != NULL)
+	{
+		rightHeight = check(node->right, newHeight);
+	}
+	if (leftHeight > rightHeight)
+	{
+		return (leftHeight);
+	}
+	return (rightHeight);
+}
+
+/**
  * binary_tree_height - returns the height of the tree
  * @tree: the tree to be checked
  * Return: an integer
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t leftHeight;
-	size_t rightHeight;
-
-	leftHeight = 0;
-	rightHeight = 0;
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	else
-	{
-		leftHeight = binary_tree_height(tree->left);
-		rightHeight = binary_tree_height(tree->right);
-
-		if (leftHeight >= rightHeight)
-		{
-			return (leftHeight + 1);
-		}
-		else
-		{
-			return (rightHeight + 1);
-		}
-	}
+	return (check(tree, 0));
 }
