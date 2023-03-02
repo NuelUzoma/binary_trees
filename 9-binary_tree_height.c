559124180e4d56c1f9e5fe8537f4c33c4d1ab7da
NuelUzoma <emcool17@gmail.com>
2023-03-02 19:18:29 +0100

    Code Test: Task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_height - returns the height of the tree
+ * @tree: the tree to be checked
+ * Return: an integer
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t leftHeight;
+	size_t rightHeight;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		leftHeight = binary_tree_height(tree->left);
+		rightHeight = binary_tree_height(tree->right);
+
+		if (leftHeight > rightHeight)
+		{
+			return (leftHeight + 1);
+		}
+		else
+		{
+			return (rightHeight + 1);
+		}
+	}
+}
