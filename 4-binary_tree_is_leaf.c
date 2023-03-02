4a9f48b414c25470cb662285136c9b9f9232de8a
NuelUzoma <emcool17@gmail.com>
2023-03-02 17:10:17 +0100

    Code Fix: Task 4 and Task 5

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,17 +8,16 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (node == NULL)
+	{
+		return (0);
+	}
 
-	if (!node)
+	if (node->left == NULL && node->right == NULL)
 	{
 		return (1);
 	}
-    else
-    {
-        return (0);
-    }
-
-	if (node == NULL)
+	else
 	{
 		return (0);
 	}
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,16 +8,16 @@
 */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (!node)
+	if (node == NULL)
+	{
+		return (0);
+	}
+	
+	if (!node->parent)
 	{
 		return (1);
 	}
 	else
-	{
-		return (0);
-	}
-
-	if (node == NULL)
 	{
 		return (0);
 	}
