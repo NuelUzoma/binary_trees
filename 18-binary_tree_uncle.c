a480562236821697c893b27468fcf5c7cb2cab83
NuelUzoma <emcool17@gmail.com>
2023-03-03 12:25:41 +0100

    Task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -23,7 +23,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	if (grandparent->left == NULL && grandparent->right == NULL)
+	if (grandparent->left == NULL)
+	{
+		return (NULL);
+	}
+	if (grandparent->right == NULL)
 	{
 		return (NULL);
 	}
