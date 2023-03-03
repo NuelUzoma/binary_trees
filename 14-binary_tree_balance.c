570cca13b4372d73736da05edb4b1968d289d34f
NuelUzoma <emcool17@gmail.com>
2023-03-03 01:35:58 +0100

    Code Test: Task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
+/**
+ * binary_tree_balance - finds the balance factor of a tree
+ * @tree: the tree to be checked
+ * Return: an integer
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t leftSubtree;
+	size_t rightSubtree;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if ((tree->left == NULL && tree->right != NULL)
+			|| (tree->left != NULL && tree->right == NULL))
+	{
+		return (-1);
+	}
+	else
+	{
+		leftSubtree = binary_tree_height(tree->left);
+		rightSubtree = binary_tree_height(tree->right);
+
+		return (leftSubtree - rightSubtree);
+	}
+}
